03a0a0af96ec810e06e848a11e71045481ae8869
Markynice <35926998+Markynice@users.noreply.github.com>
2018-01-29 21:49:40 +0200

    Update main_sitenew.py

--- ff_test_answer_pages/main_sitenew.py
+++ ff_test_answer_pages/main_sitenew.py
@@ -7,7 +7,7 @@ class MainSite(unittest.TestCase):
 	ArrayLink = ["https://preprod.fashion-flash.de","https://preprod.fashion-flash.de/wie","https://preprod.fashion-flash.de/kooperationen","https://preprod.fashion-flash.de/presse","https://preprod.fashion-flash.de/jobs","https://preprod.fashion-flash.de/kontakt"]
 	def test_Main_page(self):
 		for Links in ArrayLink:
-			Link = requests.get(Links, auth=HTTPBasicAuth('aily.team', 'e,theyc33'))
+			Link = requests.get(Links, auth=HTTPBasicAuth('', ''))
 			Status = Link.status_code
 			self.assertEqual(Status,200)
 
