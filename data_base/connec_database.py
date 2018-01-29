7e669cff9b770d863060dabfa9e19c233d418898
Markynice <35926998+Markynice@users.noreply.github.com>
2018-01-29 21:46:00 +0200

    Update connec_database.py

--- data_base/connec_database.py
+++ data_base/connec_database.py
@@ -1,5 +1,5 @@
 import mysql
 
-cnx = mysql.connector.connect(user='ff_uat', password='nbvcthdth33', host='178.159.37.41', database='ff_uat')
+cnx = mysql.connector.connect(user='ff_uat', password='', host='', database='')
 
 cnx.close()
