5290657934807a9539025aac5f558715f0fb0810
Markynice <aily.teamqa@gmail.com>
2018-01-29 19:28:49 +0200

    New file

---
+++ ff_test_answer_pages/main_sitenew.py
@@ -0,0 +1,15 @@
+import unittest
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+class MainSite(unittest.TestCase):
+	ArrayLink = ["https://preprod.fashion-flash.de","https://preprod.fashion-flash.de/wie","https://preprod.fashion-flash.de/kooperationen","https://preprod.fashion-flash.de/presse","https://preprod.fashion-flash.de/jobs","https://preprod.fashion-flash.de/kontakt"]
+	def test_Main_page(self):
+		for Links in ArrayLink:
+			Link = requests.get(Links, auth=HTTPBasicAuth('aily.team', 'e,theyc33'))
+			Status = Link.status_code
+			self.assertEqual(Status,200)
+
+if __name__ == "__main__":
+	unittest.main()
