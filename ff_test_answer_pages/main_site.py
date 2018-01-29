4aa2c2771f4d72557dff2e454ec5c1dc8fbe3c2a
Markynice <35926998+Markynice@users.noreply.github.com>
2018-01-29 21:48:52 +0200

    Update main_site.py

--- ff_test_answer_pages/main_site.py
+++ ff_test_answer_pages/main_site.py
@@ -6,58 +6,58 @@ import HtmlTestRunner
 class MainSite(unittest.TestCase):
         
         def test_Main_page(self):
-            Link = requests.get("https://preprod.fashion-flash.de", auth=HTTPBasicAuth('aily.team', 'e,theyc33'))
+            Link = requests.get("https://preprod.fashion-flash.de", auth=HTTPBasicAuth('', ''))
             Status = Link.status_code
             self.assertEqual(Status,200)
 
         def test_Unser_Event_Konzept(self):
-            Link = requests.get("https://preprod.fashion-flash.de/wie", auth=HTTPBasicAuth('aily.team', 'e,theyc33'))
+            Link = requests.get("https://preprod.fashion-flash.de/wie", auth=HTTPBasicAuth('', ''))
             Status = Link.status_code
             self.assertEqual(Status,200)
 
         def test_uber_uns(self):
-            Link = requests.get("https://preprod.fashion-flash.de/uber_uns", auth=HTTPBasicAuth('aily.team', 'e,theyc33'))
+            Link = requests.get("https://preprod.fashion-flash.de/uber_uns", auth=HTTPBasicAuth('', ''))
             Status = Link.status_code
             self.assertEqual(Status,200)
 
 
         def test_kooperationen(self):
-            Link = requests.get("https://preprod.fashion-flash.de/kooperationen", auth=HTTPBasicAuth('aily.team', 'e,theyc33'))
+            Link = requests.get("https://preprod.fashion-flash.de/kooperationen", auth=HTTPBasicAuth('', ''))
             Status = Link.status_code
             self.assertEqual(Status,200)
 
         def test_presse(self):
-            Link = requests.get("https://preprod.fashion-flash.de/presse", auth=HTTPBasicAuth('aily.team', 'e,theyc33'))
+            Link = requests.get("https://preprod.fashion-flash.de/presse", auth=HTTPBasicAuth('', ''))
             Status = Link.status_code
             self.assertEqual(Status,200)
 
         def test_jobs(self):
-            Link = requests.get("https://preprod.fashion-flash.de/jobs", auth=HTTPBasicAuth('aily.team', 'e,theyc33'))
+            Link = requests.get("https://preprod.fashion-flash.de/jobs", auth=HTTPBasicAuth('', ''))
             Status = Link.status_code
             self.assertEqual(Status,200)
 
         def test_Main_kontakt(self):
-            Link = requests.get("https://preprod.fashion-flash.de/kontakt", auth=HTTPBasicAuth('aily.team', 'e,theyc33'))
+            Link = requests.get("https://preprod.fashion-flash.de/kontakt", auth=HTTPBasicAuth('', ''))
             Status = Link.status_code
             self.assertEqual(Status,200)
 
         def test_Impressum(self):
-            Link = requests.get("https://preprod.fashion-flash.de/impressum#impressum", auth=HTTPBasicAuth('aily.team', 'e,theyc33'))
+            Link = requests.get("https://preprod.fashion-flash.de/impressum#impressum", auth=HTTPBasicAuth('', ''))
             Status = Link.status_code
             self.assertEqual(Status,200)
 
         def test_agb(self):
-            Link = requests.get("https://preprod.fashion-flash.de/agb#agb", auth=HTTPBasicAuth('aily.team', 'e,theyc33'))
+            Link = requests.get("https://preprod.fashion-flash.de/agb#agb", auth=HTTPBasicAuth('', ''))
             Status = Link.status_code
             self.assertEqual(Status,200)
 
         def test_Main_page(self):
-            Link = requests.get("https://preprod.fashion-flash.de/datenschutzerklarung#daten", auth=HTTPBasicAuth('aily.team', 'e,theyc33'))
+            Link = requests.get("https://preprod.fashion-flash.de/datenschutzerklarung#daten", auth=HTTPBasicAuth('', ''))
             Status = Link.status_code
             self.assertEqual(Status,200)
 
         def test_tickets(self):
-            Link = requests.get("https://preprod.fashion-flash.de/tickets", auth=HTTPBasicAuth('aily.team', 'e,theyc33'))
+            Link = requests.get("https://preprod.fashion-flash.de/tickets", auth=HTTPBasicAuth('', ''))
             Status = Link.status_code
             self.assertEqual(Status,200)
 
